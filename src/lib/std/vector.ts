d7fedc1fac15a5e8b0973c302c2f244b309c3139
spasimir21 <spasimir.pavlov@gmail.com>
2026-01-11 02:52:50 +0200

    More vector stuff

--- src/lib/std/vector.ts
+++ src/lib/std/vector.ts
@@ -3,8 +3,8 @@
 'use raw';
 
 import { RawArray, RawPointer, RawTypeContainer, Struct, UInt32, Void } from '../types';
-import { free, M_U8, malloc, mresize, NULL_PTR } from '../runtime';
-import { sizeOf$ } from '../macros';
+import { free, M, M_U8, malloc, mresize, NULL_PTR } from '../runtime';
+import { pointerCast$, sizeOf$ } from '../macros';
 
 type Vector<T extends RawTypeContainer> = Struct<{
   length: UInt32;
@@ -29,6 +29,7 @@ function vector_init(typedVector: Vector<RawTypeContainer>, valueSize: number, c
   const vector = typedVector as any as UntypedVector;
 
   vector.length = 0 as UInt32;
+  vector.capacity = 0 as UInt32;
   vector.valueSize = valueSize as UInt32;
 
   vector.data = NULL_PTR as RawPointer<Void>;
@@ -40,6 +41,7 @@ function vector_deinit(typedVector: Vector<RawTypeContainer>): void {
 
   if (vector.data !== NULL_PTR) free(vector.data);
 
+  vector.valueSize = 0 as UInt32;
   vector.length = 0 as UInt32;
   vector.capacity = 0 as UInt32;
   vector.data = NULL_PTR as RawPointer<Void>;
@@ -54,6 +56,7 @@ function vector_scale(typedVector: Vector<RawTypeContainer>, capacity: number): 
   const oldSize = vector.capacity * vector.valueSize;
   const newSize = capacity * vector.valueSize;
 
+  vector.length = Math.min(vector.length, capacity) as UInt32;
   vector.capacity = capacity as UInt32;
 
   if (vector.data === NULL_PTR) {
@@ -77,4 +80,41 @@ function vector_scale(typedVector: Vector<RawTypeContainer>, capacity: number): 
   free(oldData);
 }
 
-export { VECTOR_SIZE, vector_init, vector_deinit, vector_scale, Vector, UntypedVector };
+function vector_ensure_capacity(typedVector: Vector<RawTypeContainer>, targetCapacity: number): void {
+  const vector = typedVector as any as UntypedVector;
+  if (vector.capacity >= targetCapacity) return;
+
+  let capacity: number = vector.capacity;
+  while (capacity < targetCapacity) capacity = capacity == 0 ? 1 : capacity * 2;
+
+  vector_scale(typedVector, capacity);
+}
+
+function vector_push<T extends RawTypeContainer>(typedVector: Vector<T>): RawPointer<T> {
+  const vector = typedVector as any as UntypedVector;
+
+  vector_ensure_capacity(typedVector, vector.length + 1);
+  vector.length++;
+
+  return (vector.data + vector.valueSize * (vector.length - 1)) as RawPointer<T>;
+}
+
+// vector_at
+// vector_splice
+// vector_pop
+// vector_shift
+// vector_unshift
+// vector_clear
+// vector_delete
+// vector_insert
+
+export {
+  VECTOR_SIZE,
+  vector_init,
+  vector_deinit,
+  vector_scale,
+  vector_ensure_capacity,
+  vector_push,
+  Vector,
+  UntypedVector
+};
--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,7 @@
 'use raw';
 
+import { Vector, vector_deinit, vector_init, vector_push, VECTOR_SIZE } from './lib/std';
 import { free, getMemoryAnalysis, initializeMemory, M, malloc } from './lib/runtime';
-import { Vector, vector_deinit, vector_init, VECTOR_SIZE } from './lib/std';
 import { addressOf$, sizeOf$ } from './lib/macros';
 import { UInt32 } from './lib/types';
 
@@ -9,13 +9,10 @@ initializeMemory();
 
 const vector = malloc<Vector<UInt32>>(VECTOR_SIZE).value$;
 
-vector_init(vector, sizeOf$<UInt32>(), 16);
-
-const data = vector.data.value$;
+vector_init(vector, sizeOf$<UInt32>(), 0);
 
 for (let i = 0; i < 12; i++) {
-  data[i] = i as UInt32;
-  vector.length++;
+  vector_push(vector).value$ = i as UInt32;
 }
 
 const view = new Uint32Array(M, addressOf$(vector) - 8, 64);
